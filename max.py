ac2fda4661650b3be6922afc1697824307628831
Gene Blanchard <gblanchard4@gmail.com>
2014-10-13 11:14:24 -0500

    Added star section with working config

--- max.py
+++ max.py
@@ -2,11 +2,9 @@
 
 import argparse
 import os
-# STAR module
-import star
-# Hrrmmmm Do I want to go this route now?
 import ConfigParser
-
+from multiprocessing import cpu_count
+import errno
 
 __author__ = "Gene Blanchard"
 __email__ = "me@geneblanchard.com"
@@ -18,6 +16,38 @@ See README.md
 
 '''
 
+def star_command_builder(config, read_1, read_2, threads, output):
+	# Read the Star parameters, set defaults if none are listed
+	index = config.get('star', 'genome_dir')
+	clip5p = config.get('star', 'clip5pNbases')	
+	repeat = config.get('star', 'outFilterMultimapNmax')
+	
+	# Base string
+	command_string = "STAR"
+	# Index/Genome Location
+	command_string += " --genomeDir {}".format(index)
+	# Number of bases to clip
+	command_string += "--clip5pNbases {}".format(clip5p)
+	# outFilterMultimapNmax
+	command_string += " --outFilterMultimapNmax {}".format(repeat)
+	# Buffer Limit 
+	command_string += " --limitIObufferSize 2750000000"
+	# Input files NOTE: Will make future change for non-paired data
+	command_string += " --readFilesIn {} {}".format(read_1, read_2)
+	# Gunzip only if gz'ed
+	if read_1.endswith('.gz'):
+		command_string += " --readFilesCommand gunzip -c"
+	# outReadsUnmapped
+	command_string += " --outReadsUnmapped Fastx"
+	# Number of processors
+	command_string += " --runThreadN {}".format(threads)
+	# Output string
+	command_string += " --outFileNamePrefix {}".format(output)
+	# end of line
+	command_string += ";\n"
+
+	print command_string
+	return command_string
 
 def main():
 
@@ -46,7 +76,8 @@ def main():
 	reverse_path = os.path.abspath(args.reverse)
 	output_dir = os.path.abspath(args.output)
 	config_file = os.path.abspath(args.config)
-	threads = arga.threads
+	threads = args.processors
+
 	# Error Checks
 	ERROR = False
 
@@ -60,14 +91,17 @@ def main():
 		print "Reverse file not found"
 	# Test write permissions of output
 	try:
-		os.makedirs(path)
+		os.makedirs(output_dir)
 	except OSError as exception:
 		if exception.errno != errno.EEXIST:
 			raise
 
+	# Read the config file
+	config = ConfigParser.RawConfigParser()
+	config.read(config_file)
 
-
-
+	# STAR
+	star_command_builder(config, forward_path, reverse_path, threads, output_dir)
 
 if __name__ == '__main__':
 	main()
